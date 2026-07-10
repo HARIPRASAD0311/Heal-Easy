bf959dba718a8728b5bf25bb94145b83669c2ceb
HARIPRASAD0311 <hariprasad03official@gmail.com>
2026-07-10 11:33:10 +0530

    Fix: remove broken amplify import in main.jsx, fix amplify.yml cd commands

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,7 @@
-import "./amplify";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
-// Configure AWS Amplify before React renders so auth state is
-// available across the entire app without a Provider wrapper.
 import configureAmplify from "./config/amplify.js";
 configureAmplify();
 
